0d50e27b4eda003cb2274a19d51e25a0bf82528e
Emily Sá <fc58200@alunos.fc.ul.pt>
2023-05-20 19:05:53 +0100

    changes to read_args

--- ADMPOR/src/configuration.c
+++ ADMPOR/src/configuration.c
@@ -3,9 +3,15 @@
 #include "configuration.h"
 #include "main.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+#include "configuration.h"
+#include "main.h"
+
 void read_arguments(char *file_name, struct main_data *data)
 {
     FILE *file = fopen(file_name, "r");
+    char contents[100];
 
     if (file == NULL)
     {
@@ -13,14 +19,17 @@ void read_arguments(char *file_name, struct main_data *data)
         exit(0);
     }
 
-    fscanf(file, "%d", &(data->max_ops));
-    fscanf(file, "%d", &(data->buffers_size));
-    fscanf(file, "%d", &(data->n_clients));
-    fscanf(file, "%d", &(data->n_intermediaries));
-    fscanf(file, "%d", &(data->n_enterprises));
-    fscanf(file, "%s", &(data->log_filename));
-    fscanf(file, "%s", &(data->statistics_filename));
-    fscanf(file, "%d", &(data->alarm_time));
+    data->max_ops = *fgets(contents, 100, file);
+    data->buffers_size = *fgets(contents, 100, file);
+    data->n_clients = *fgets(contents, 100, file);
+    data->n_intermediaries = *fgets(contents, 100, file);
+    data->n_enterprises = *fgets(contents, 100, file);
+    data->log_filename = fopen(fgets(contents, 100, file), "w+");
+    data->statistics_filename = fopen(fgets(contents, 100, file), "w+");
+    int alarm = *fgets(contents, 100, file);
+    data->alarm_time = &alarm;
 
     fclose(file);
+
+    data->terminate = 0;
 }
